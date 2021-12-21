6b849b1ffafcdfffb57c33b243e00a83c3638a9d
zhangrui <1193405871@qq.com>
2021-12-21 13:43:00 +0800

    add new

---
+++ 最终代码/STMS/STMS.STMSApp/LogHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STMS.STMSApp
+{
+    class LogHelper
+    {
+        private static string path = "./Logs";
+        public static void Write(string msg)
+        {
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            File.AppendAllText(path + "./Log.txt", $"{DateTime.Now}:{msg}\r\n");
+        }
+    }
+}
---
+++ 最终代码/STMS/STMS.STMSApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STMS.STMSApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            LogHelper.Write("测试txt");
+            LogHelper.Write("测试完美解决");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
